0848e641ba909fdf3557638841b0b90a3649baea
Dia Pal <diap76046@gmail.com>
2026-02-14 00:54:38 +0530

    Add files via upload

---
+++ class.py
@@ -0,0 +1,16 @@
+class employee():
+    salary = 1200000
+    language = "python"
+# here language and salary are the class attributes
+e1 = employee()
+e1.name = "Harry"
+print(e1.name)
+print(e1.language)
+print(e1.salary)
+
+rohan = employee()
+rohan.name = "dia pal"
+print(rohan.name,rohan.language)
+
+# here name is object oriented attribute and language and  salary are the class attributes
+
