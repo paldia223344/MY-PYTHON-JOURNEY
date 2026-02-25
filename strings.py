0ca10b1b6791aa585fdb208445c4be5dc8a6d3b5
Dia Pal <diap76046@gmail.com>
2026-02-25 16:55:18 +0530

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,16 @@
+# string = "He is persuing his b.tech from iit bombay"
+# # print(string.find("is")) # gives the index of the word found at which index
+# # print(string.endswith("his",5,"bombay"))
+# # print(string.inex("his"))
+# print(string.capitalize())
+# string2= "hidjbkv"
+# print(string2.isalnum())
+# print(string2.isalpha())
+# print(string2.islower())
+# print(string2.isprintable())
+# print(string2.swapcase())
+# print(string2.title())
+
+string = "my name is {} and iam a billionary"
+name= " Dia Pal"
+print(string.format(name))
