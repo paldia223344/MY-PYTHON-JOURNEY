e3a5bb06ba2c14734cd6d02e19d67c335cc7ff39
Dia Pal <diap76046@gmail.com>
2026-02-25 16:54:58 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,20 @@
+# x = int(input("Enter a numbe x"))
+
+# match x:
+#     case 0:
+#         print("case 0 ")
+#     case 7:
+#         print("case 7")
+#     case _:
+#         print("___")
+        
+#WHILE LOOP
+i = int(input("Enter a number"))
+while(i<=89):
+    i = int(input("Enter a number"))
+    print(i)
+    
+        
+print("loop is done")
+       
+        
