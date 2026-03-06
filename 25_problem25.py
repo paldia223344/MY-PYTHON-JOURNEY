659ab9ca3aef8a41b207834f95b3e9d2be7a3f3e
Dia Pal <diap76046@gmail.com>
2026-03-06 23:58:21 +0530

    Add files via upload

---
+++ 25_problem25.py
@@ -0,0 +1,23 @@
+'''
+Write a program to print the following start pattern using the for loop
+
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+* * * * 
+* * * 
+* * 
+*
+'''
+rows = int(input("Enter number of rows"))
+for i in range (0,rows):
+    for j in range(0,i+1):
+       print("*" ,end="")
+    print("\r")
+    
+for i in range (rows,0,-1):
+    for j in range(0,i-1):
+       print("*" ,end="")
+    print("\r")
