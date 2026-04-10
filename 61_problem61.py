a3b1ead419a8164d4a2dca4112cf42b0027ca31e
Dia Pal <diap76046@gmail.com>
2026-04-10 10:00:34 +0530

    Add files via upload

---
+++ 61_problem61.py
@@ -0,0 +1,8 @@
+from tkinter import*
+alia_root = Tk()
+alia_root.geometry("400x200")
+alia_root.minsize(200,100)
+alia_root.maxsize(1920,999)
+dia = Label(text = "A SAMPLE TO PRESENT")
+dia.pack()
+alia_root.mainloop()
