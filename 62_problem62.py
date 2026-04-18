2eb5a486be1e6f1ba881bff084c5b47726462c42
Dia Pal <diap76046@gmail.com>
2026-04-18 10:22:42 +0530

    Add files via upload

---
+++ 62_problem62.py
@@ -0,0 +1,6 @@
+from tkinter import *
+root = Tk()
+root.geometry("400x1200")
+label = Label(text = '''Good Morning Everyone''',bg = "purple", fg = "blue", font = "comicsansms 20 bold", padx = 10 , pady = 20, borderwidth  = 5 , relief = SUNKEN)
+label.pack()
+root.mainloop()
