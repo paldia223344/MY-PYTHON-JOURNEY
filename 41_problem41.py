8bcaa5a5f333e2baf1244ed721a6166557c05d44
Dia Pal <diap76046@gmail.com>
2026-04-28 19:39:46 +0530

    Add files via upload

---
+++ 41_problem41.py
@@ -0,0 +1,13 @@
+'''
+ Flatten a nested list using loops
+Write a program to flatten a nested list using loops.
+'''
+# nested_list= [1, [2, 3], [4, 5, 6], 7, [8, 9]]
+# flattened = flatten_list(nested_list)
+# print(f"Flattened list will be : {flattened}")
+
+nested_list = [1, [2, 3], [4, 5, 6], 7, [8, 9]]
+
+# write function 'flatten_list' to flatten a nested list
+#flattened = flatten_list(nested_list)
+#print("Flattened list:", flattened)
