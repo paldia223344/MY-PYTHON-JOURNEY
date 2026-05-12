33f5b3ce270784fec8011a2cf34b1af6dd52ee43
Dia Pal <diap76046@gmail.com>
2026-05-13 01:48:24 +0530

    Add files via upload

---
+++ 74_problem74.py
@@ -0,0 +1,11 @@
+class Dog:
+    species = "Canine" # class attribute 
+    
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age 
+        
+dog1 = Dog("Buddy" , 3)
+
+print(dog1.name)
+print(dog1.species)
